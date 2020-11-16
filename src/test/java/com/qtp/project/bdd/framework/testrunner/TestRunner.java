b811dd500f83c0cf92d6d5ce2d48e531b3504780
amrutha sireesh <amrutha.sireesh@co-cc02846.ad.cabinetoffice.gov.uk>
2020-11-16 09:03:38 +0000

    remove cucumber tags

--- src/test/java/com/qtp/project/bdd/framework/testrunner/TestRunner.java
+++ src/test/java/com/qtp/project/bdd/framework/testrunner/TestRunner.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources",
-        glue = {"com.qtp.project.bdd.framework.stepdefinitions"},
-        tags = "@searchFramework or @axe"
+        glue = {"com.qtp.project.bdd.framework.stepdefinitions"}
+    //    tags = "@searchFramework or @axe"
         )
 public class TestRunner {
 
