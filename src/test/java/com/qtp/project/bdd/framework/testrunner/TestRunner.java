b012838dd572bf3a779363bc9b26c84d87455d71
amrutha sireesh <amrutha.sireesh@co-cc02846.ad.cabinetoffice.gov.uk>
2020-11-25 10:37:09 +0000

    git ignore

--- src/test/java/com/qtp/project/bdd/framework/testrunner/TestRunner.java
+++ src/test/java/com/qtp/project/bdd/framework/testrunner/TestRunner.java
@@ -8,10 +8,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources",
+        publish = true,
         glue = {"com.qtp.project.bdd.framework.stepdefinitions"},
        // plugin = { "de.monochromata.cucumber.report.PrettyReports:target/cucumber" },
         plugin = {"pretty", "html:target/report/cucumber.html", "json:target/report/cucumber.json", "junit:target/report/cucumber.xml"},
-        tags = "@cctv"
+        tags = "@reg"
         //mvn test -Dcucumber.filter.tags="@cctv"
         )
 public class TestRunner {
